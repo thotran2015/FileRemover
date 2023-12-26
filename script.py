4e0e6253dffc6639c9adb068333a5edfe2ae50e8
Tran, Tho <Tho.Tran@netapp.com>
2023-12-26 18:03:44 -0500

    move old files to trash

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ import platform
 MAC_OS = "Darwin"
 
 
-def notify(title, msg):
+def create_dialog(title, msg):
     current_os = platform.system()
     if current_os == MAC_OS:  # macOS
         # Use AppleScript to create interactive notification for file deletion warning
@@ -16,7 +16,31 @@ def notify(title, msg):
     return 1
 
 
+def create_notification(title, msg):
+    current_os = platform.system()
+    if current_os == MAC_OS:
+        subprocess.run(['osascript', '-e', f'display notification "{msg}" with title "{title}"'])
+
+
+def move_files_to_trash(directory: str, days_old: int, trash_path: str):
+    if not os.path.exists(directory):
+        print(f"directory does not exist: {directory}")
+        return
+    cur_time = time.time()
+    num_files_moved = 0
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        file_age_secs = cur_time - os.path.getatime(filepath)
+        if file_age_secs/(3600*24) > days_old:
+            shutil.move(filepath, trash_path)
+            num_files_moved += 1
+    create_notification("Files Moved to Trash", f'{num_files_moved} files are moved from {directory} to trash')
+
+
 def delete_old_files(directory: str, days_old: int):
+    if not os.path.exists(directory):
+        print(f"directory does not exist: {directory}")
+        return
     files_to_delete = []
     directories_to_delete = []
     cur_time = time.time()
@@ -32,7 +56,7 @@ def delete_old_files(directory: str, days_old: int):
     # Notify users before delete files
     title = f"Warning: About to delete {len(files_to_delete)} files in {directory}"
     msg = f"Do you want to delete files {files_to_delete}?"
-    user_res = notify(title, msg)
+    user_res = create_dialog(title, msg)
     if user_res == 0:
         print(f"Deleting files {files_to_delete}")
         for file in files_to_delete:
@@ -42,7 +66,7 @@ def delete_old_files(directory: str, days_old: int):
     # Notify users before delete directories
     title = f"Warning: About to delete {len(directories_to_delete)} subdirectories in {directory}"
     msg = f"Do you want to delete directories {directories_to_delete}?"
-    user_res = notify(title, msg)
+    user_res = create_dialog(title, msg)
     if user_res == 0:
         print(f"Deleting directories {directories_to_delete}")
         for d in directories_to_delete:
@@ -51,9 +75,14 @@ def delete_old_files(directory: str, days_old: int):
 
 
 def main():
-    download_directory = "/Users/thotran/Downloads"
+    home_directory = os.path.expanduser('~')
+    trash_path = os.path.join(home_directory, ".Trash")
+    download_directory = os.path.join(home_directory, "Download")
+    desktop_directory = os.path.join(home_directory, "Desktop")
     days_threshold = 7
-    delete_old_files(download_directory, days_threshold)
+    # delete_old_files(download_directory, days_threshold)
+    move_files_to_trash(download_directory, days_threshold, trash_path)
+    move_files_to_trash(desktop_directory, days_threshold, trash_path)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -3,18 +3,21 @@ import unittest
 import tempfile
 import os
 from datetime import datetime, timedelta
-from script import delete_old_files, notify, MAC_OS
+from script import delete_old_files, create_dialog, MAC_OS, move_files_to_trash
 from unittest import mock
 import platform
 
 
-class TestFileRemoverMethods(unittest.TestCase):
+class TestScript(unittest.TestCase):
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
+        self.test_trash = tempfile.mkdtemp()
 
     def tearDown(self):
         if os.path.exists(self.test_dir):
             shutil.rmtree(self.test_dir)
+        if os.path.exists(self.test_trash):
+            shutil.rmtree(self.test_trash)
 
     def create_test_file(self, filename, days_old):
         filepath = os.path.join(self.test_dir, filename)
@@ -25,19 +28,19 @@ class TestFileRemoverMethods(unittest.TestCase):
         return filepath
 
     @mock.patch('subprocess.run')
-    def test_notify(self, mock_subprocess_run):
+    def test_dialog(self, mock_subprocess_run):
         if platform.system() == MAC_OS:
             title = "Warning: File Deletion in 10s",
             msg = f"About to delete file2.txt",
-            notify(title, msg)
+            create_dialog(title, msg)
             expected_cmd = ['osascript', '-e', f'display dialog "{msg}" buttons {{"Cancel", "OK"}} default button "OK" with title "{title}"']
             mock_subprocess_run.assert_called_once_with(expected_cmd)
 
-    @mock.patch('script.notify')
-    def test_delete_old_files(self, mock_notify):
+    @mock.patch('script.create_dialog')
+    def test_delete_old_files(self, mock_create_dialog):
         file1 = self.create_test_file("file1.txt", 5)
         file2 = self.create_test_file("file2.txt", 8)
-        mock_notify.return_value = 0
+        mock_create_dialog.return_value = 0
         delete_old_files(self.test_dir, days_old=7)
 
         if platform.system() == MAC_OS:
@@ -47,11 +50,28 @@ class TestFileRemoverMethods(unittest.TestCase):
             title2 = f'Warning: About to delete 0 subdirectories in {self.test_dir}'
             msg2 = "Do you want to delete directories []?"
             notify_call2 = mock.call(title2, msg2)
-            mock_notify.assert_has_calls([notify_call1, notify_call2], any_order=False)
+            mock_create_dialog.assert_has_calls([notify_call1, notify_call2], any_order=False)
 
         # Make sure file1 is not deleted but file2 is
         self.assertTrue(os.path.exists(file1))
         self.assertFalse(os.path.exists(file2))
+
+    @mock.patch('script.create_notification')
+    @mock.patch('shutil.move')
+    def test_move_files_to_trash(self, mock_move, mock_create_notification):
+        file1 = self.create_test_file("file1.txt", 5)
+        file2 = self.create_test_file("file2.txt", 8)
+        home_directory = os.path.expanduser('~')
+        trash_path = os.path.join(home_directory, ".Trash")
+        move_files_to_trash(self.test_dir, 7, trash_path)
+        mock_move.assert_called_once_with(file2, trash_path)
+
+        if platform.system() == MAC_OS:
+            title = "Files Moved to Trash"
+            msg = f'1 files are moved from {self.test_dir} to trash'
+            mock_create_notification.assert_called_once_with(title, msg)
+        # make sure only file 2 is moved
+        self.assertTrue(os.path.exists(file1))
 
 
 if __name__ == "__main__":
